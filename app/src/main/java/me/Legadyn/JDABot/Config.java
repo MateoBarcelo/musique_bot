6f0442616bf8d677f6d533ed8914b50b16580359
Legadyn <killerwill888@gmail.com>
2021-09-28 13:08:38 -0300

    Update config

--- app/src/main/java/me/Legadyn/JDABot/Config.java
+++ app/src/main/java/me/Legadyn/JDABot/Config.java
@@ -1,17 +1,13 @@
 package me.Legadyn.JDABot;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 public class Config {
-
-    private static final Dotenv dotenv = Dotenv.load();
 
     public static String get(ConfigKeys key) {
 
         switch (key) {
-            case TOKEN: return dotenv.get("TOKEN");
-            case PREFIX: return dotenv.get("PREFIX");
-            case CREADOR: return dotenv.get("CREADOR");
+            case TOKEN: return System.getenv().get("TOKEN");
+            case PREFIX: return System.getenv().get("PREFIX");
+            case CREADOR: return System.getenv().get("CREADOR");
             default: break;
         }
         return null;
